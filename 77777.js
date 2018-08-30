6519b0b054ba861e76e7d138d89b1b2c99e52437
galychat <42301824+galychat@users.noreply.github.com>
2018-08-30 15:00:00 +0300

    Create 77777.js

---
+++ 77777.js
@@ -0,0 +1,73 @@
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`
+   
+`).appendTo('div#d2')
+			clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+		
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`<style>div#d0 span {
+        background-color: #91817D;
+        border: 1px solid #FFFFFA;
+
+}
+
+.ae.label.label-primary.fa.fa-gear{border: 1px solid #91817D;border-radius:7px 7px 15px 15px}
+.ae.fa.chats.label.fa-comment.label-warning{border-bottom: 1px solid #D14545;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #A6FFFF;margin-left:-1px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.fa-commenting-o.label-primary{border-bottom: 1px solid #F660AB;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.fa-commenting-o.label-warning{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.chats.label.fa-comment.label-primary{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.label-primary.fa-users{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;border-radius:7px 0px 0px 15px}
+.ae.fa.label.label-primary.fa-user{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;border-radius:7px 0px 0px 15px}
+
+</style>
+`).insertBefore('body'),
+				clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`<center></i><audio controls="" loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio>
+</div></i> <iframe src="https://tunein.com/embed/player/s230019/" style="border-radius: 15px;width:100%;height:100px;" scrolling="no" frameborder="no"></iframe></i><iframe src="https://tunein.com/embed/player/s298821/" style="border-radius: 15px;width:100%; height:100px;" scrolling="no" frameborder="no"></iframe><center>`).appendTo('div#chats')
+			clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
