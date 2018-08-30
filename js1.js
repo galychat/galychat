0504632af88ae9adff0fc6d41f753f6066b5a5f0
galychat <42301824+galychat@users.noreply.github.com>
2018-08-30 14:33:00 +0300

    Create js1.js

---
+++ js1.js
@@ -0,0 +1,84 @@
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://galychat.com/sico/z1cm597q7a10.png);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="sico/z1cfan45b210.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"مرحبا بكم في شات غالي 1/span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl">ظ‹ ط‘‹ <h1 style="display: inline-block;color: red;">` + getuser(myid).topic + `</h1>شات غالي2</div>
+      </div>
+    </div></div>
+`).appendTo('div#d2')
+			clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+		
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`<style>div#d0 span {
+        background-color: #91817D;
+        border: 1px solid #FFFFFA;
+
+}
+
+.ae.label.label-primary.fa.fa-gear{border: 1px solid #91817D;border-radius:7px 7px 15px 15px}
+.ae.fa.chats.label.fa-comment.label-warning{border-bottom: 1px solid #D14545;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #A6FFFF;margin-left:-1px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.fa-commenting-o.label-primary{border-bottom: 1px solid #F660AB;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.fa-commenting-o.label-warning{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.chats.label.fa-comment.label-primary{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.label-primary.fa-users{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;border-radius:7px 0px 0px 15px}
+.ae.fa.label.label-primary.fa-user{border-bottom: 1px solid #91817D;
+        border-top: 1px solid #91817D;
+        border-Right: 1px solid #91817D;border-left: 1px solid #91817D;border-radius:7px 0px 0px 15px}
+
+</style>
+`).insertBefore('body'),
+				clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
+$("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`<center></i><audio controls="" loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio>
+</div></i> <iframe src="https://tunein.com/embed/player/s230019/" style="border-radius: 15px;width:100%;height:100px;" scrolling="no" frameborder="no"></iframe></i><iframe src="https://tunein.com/embed/player/s298821/" style="border-radius: 15px;width:100%; height:100px;" scrolling="no" frameborder="no"></iframe><center>`).appendTo('div#chats')
+			clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+})
